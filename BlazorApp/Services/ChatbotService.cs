5153acd81f2f6b1efe69d93adc918268afcf7e0c
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2023-11-04 16:10:33 +0100

    add question

--- BlazorApp/Services/ChatbotService.cs
+++ BlazorApp/Services/ChatbotService.cs
@@ -24,6 +24,43 @@ namespace BlazorApp.Services
         {
             return _ctx.ChatBotQuestions.Where(question => question.FollowUpID == followUpID).ToList();
         }
+        
+        public void AddQuestion(ChatBotQuestion question)
+        {
+            _ctx.ChatBotQuestions.Add(question);
+            _ctx.SaveChanges();
+        }
+
+        // add follow up question
+        public void AddFollowUpQuestion(ChatBotQuestion parentQuestion, ChatBotQuestion question)
+        {
+            var dbParentQuestion = _ctx.ChatBotQuestions.Find(parentQuestion.Id);
+            var followUpID = dbParentQuestion.FollowUpQuestion;
+            
+            if(followUpID == null)
+            {
+                followUpID = GetNextFollowUpQuestionID();
+                dbParentQuestion.FollowUpQuestion = followUpID;
+            }
+
+            question.FollowUpID = followUpID;
+
+            _ctx.ChatBotQuestions.Add(question);
+            _ctx.SaveChanges();
+        }
+
+        public void EditQuestion(ChatBotQuestion question)
+        {
+            _ctx.ChatBotQuestions.Update(question);
+            _ctx.SaveChanges();
+        }
+
+        public int GetNextFollowUpQuestionID()
+        {
+            int maxFollowUpID = _ctx.ChatBotQuestions.Max(question => question.FollowUpID) ?? 0;
+            int nextFollowUpID = maxFollowUpID + 1;
+            return nextFollowUpID;
+        }
     }
 
 }
