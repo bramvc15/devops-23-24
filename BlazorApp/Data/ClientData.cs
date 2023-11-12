834138388f84276383568da48816bd37ab75b974
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-11-12 16:29:51 +0100

    validations woop woop

--- BlazorApp/Data/ClientData.cs
+++ BlazorApp/Data/ClientData.cs
@@ -1,11 +1,34 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace BlazorApp.Data
 {
     public class ClientData
     {
+        [Required (ErrorMessage = "Naam is verplicht")]
+        [StringLength(30, ErrorMessage = "Naam is te lang")]
         public string Name { get; set; }
+        [Required (ErrorMessage = "Email is verplicht")]
+        [EmailAddress(ErrorMessage = "Email is niet geldig")]
         public string Email { get; set; }
+        [Required (ErrorMessage = "Telefoon is verplicht")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number format.")]
         public string Phone { get; set; }
+        [Required (ErrorMessage = "Geboortedatum is verplicht")]
+        [DateNotInFuture(ErrorMessage = "Geboortedatum mag niet in de toekomst liggen")]
         public DateTime birthDate { get; set; }
     }
 }
+
+public class DateNotInFutureAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value is DateTime date)
+        {
+            return date <= DateTime.Now;
+        }
+
+        return true;
+    }
+}
+
