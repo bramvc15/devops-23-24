509749ece6c290659d9283b61fd82936af48ea33
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-11-12 15:28:50 +0100

    full form met css (nog geen validations)

---
+++ BlazorApp/Data/ClientData.cs
@@ -0,0 +1,11 @@
+using System;
+namespace BlazorApp.Data
+{
+    public class ClientData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime birthDate { get; set; }
+    }
+}
