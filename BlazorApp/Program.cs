85f6d2db146c4773f43093ebcd9c21b088c9c3e4
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-11-16 15:24:20 +0100

    host dingetje ip, zet et in uwen gitignore hierna

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -11,7 +11,7 @@ using BlazorApp.Auth;
 using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse("172.18.138.195"), 5046));
+builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse(builder.Configuration.GetConnectionString("HostAdress")), 5046));
 // Add services to the container.
 builder.Services.AddSingleton<BlitzWareAuthService>();
 builder.Services.AddRazorPages();
