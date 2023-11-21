ceb86a8f197770cc165f69d5f9c08b08b11f522b
Victor Heyse <victor.heyse@student.hogent.be>
2023-11-21 20:07:48 +0100

    Init Setup ScheduleTimeSlots

---
+++ BlazorApp/Controllers/ScheduleTimeSlotController.cs
@@ -0,0 +1,54 @@
+using BlazorApp.Services;
+using BlazorApp.Models;
+using BlazorApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using BlazorApp.Pages;
+
+namespace BlazorApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ScheduleTimeSlotController : ControllerBase
+{
+    ScheduleTimeSlotService _service;
+
+    public ScheduleTimeSlotController(ScheduleTimeSlotService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    [Route("getScheduleTimeSlots")]
+    public Task<List<ScheduleTimeSlot>> GetScheduleTimeSlots()
+    {
+        return _service.GetScheduleTimeSlots();
+    }
+
+    [HttpGet]
+    [Route("getScheduleTimeSlot/{id}")]
+    public Task<ScheduleTimeSlot> GetScheduleTimeSlot(int Id)
+    {
+        return _service.GetScheduleTimeSlot(Id);
+    }
+
+    [HttpPost]
+    [Route("addScheduleTimeSlot")]
+    public void AddScheduleTimeSlot(int Id, int DoctorId, AppointmentType AppointmentType, DateTime DateTime, int Duration)
+    {
+        _service.AddScheduleTimeSlot(Id, DoctorId, AppointmentType, DateTime, Duration);
+    }
+
+    [HttpPut]
+    [Route("updateScheduleTimeSlot")]
+    public void UpdateScheduleTimeSlot(int Id, int DoctorId, AppointmentType AppointmentType, DateTime DateTime, int Duration)
+    {
+        _service.UpdateScheduleTimeSlot(Id, DoctorId, AppointmentType, DateTime, Duration);
+    }
+
+    [HttpDelete]
+    [Route("deleteScheduleTimeSlot")]
+    public void DeleteScheduleTimeSlot(int Id)
+    {
+        _service.DeleteScheduleTimeSlot(Id);
+    }
+}
---
+++ BlazorApp/Migrations/20231121175829_addScheduleTimeSlot.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addScheduleTimeSlot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentTimeSlots");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleGroup",
+                table: "ScheduleTimeSlots");
+
+            migrationBuilder.RenameColumn(
+                name: "Doctor_Id",
+                table: "ScheduleTimeSlots",
+                newName: "DoctorId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Duration",
+                table: "ScheduleTimeSlots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "ScheduleTimeSlots",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentType",
+                table: "ScheduleTimeSlots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Allergies",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BloodGroup",
+                table: "Patients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Patients",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Patients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeSlot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    AppointmentType = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AppointmentId = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSlot", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment");
+
+            migrationBuilder.DropTable(
+                name: "TimeSlot");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentType",
+                table: "ScheduleTimeSlots");
+
+            migrationBuilder.DropColumn(
+                name: "Allergies",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "BloodGroup",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Patients");
+
+            migrationBuilder.RenameColumn(
+                name: "DoctorId",
+                table: "ScheduleTimeSlots",
+                newName: "Doctor_Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Duration",
+                table: "ScheduleTimeSlots",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "ScheduleTimeSlots",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ScheduleGroup",
+                table: "ScheduleTimeSlots",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentTimeSlots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Doctor_Id = table.Column<int>(type: "int", nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Patient_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentTimeSlots", x => x.Id);
+                });
+        }
+    }
+}
