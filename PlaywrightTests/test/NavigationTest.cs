36a95842b631566685a4fefe89a5a72cc5c6860d
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2023-11-21 23:04:46 +0100

    first test

---
+++ PlaywrightTests/test/NavigationTest.cs
@@ -0,0 +1,46 @@
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class NavigationTest : PageTest
+{
+
+    string _baseUrl = "http://192.168.0.123:5046";
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        await Page.GotoAsync(_baseUrl);
+    }
+
+    [Test]
+    public async Task NavbarRedirectsToOnsTeamPage()
+    {
+        await Page.ClickAsync("text=Ons team");
+        Assert.AreEqual($"{_baseUrl}/onsTeam", Page.Url);
+    }
+
+    [Test]
+    public async Task NavbarRedirectsToBehandelingenPage()
+    {
+        await Page.ClickAsync("text=Behandelingen");
+        Assert.AreEqual($"{_baseUrl}/behandelingen", Page.Url);
+    }
+
+    [Test]
+    public async Task NavbarRedirectsToContactPage()
+    {
+        await Page.ClickAsync("text=Contact");
+        Assert.AreEqual($"{_baseUrl}/Contact", Page.Url);
+    }
+
+    [Test]
+    public async Task NotLoggedIn_AdminRedirectsToLoginPage()
+    {
+        await Page.GotoAsync($"{_baseUrl}/admin");
+        Assert.AreEqual($"{_baseUrl}/login", Page.Url);
+    }
+}
