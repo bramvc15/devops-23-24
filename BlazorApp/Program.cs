3fc55c7543782d54b50c789ec62526b0a901d1f8
Bram Van Coile <bram.vancoile@student.hogent.be>
2023-11-23 11:11:16 +0100

    Toevoegen van Syncfucion en lisencekey

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -5,10 +5,14 @@ using Blazorise.Icons.FontAwesome;
 using Blazorise.RichTextEdit;
 using Microsoft.EntityFrameworkCore;
 using BlazorApp.Services;
+using Syncfusion.Blazor;
+
 
 using static BlazorApp.Auth.BlitzWareAuth;
 using BlazorApp.Auth;
 using Blazored.LocalStorage;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIf0x0TXxbf1xzZFRGalhXTnRdUiweQnxTdEZiWH1fcXRRQGJeV0N1WQ==");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse(builder.Configuration.GetConnectionString("HostAdress")), 5046));
@@ -29,6 +33,7 @@ builder.Services.AddTransient<PatientService>();
 builder.Services.AddTransient<ScheduleTimeSlotService>(); 
 builder.Services.AddTransient<AppointmentService>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
     {
