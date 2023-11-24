4a74495643206980a8937373bfe58e336fa2481d
Victor Heyse <victor.heyse@student.hogent.be>
2023-11-24 14:32:34 +0100

    Added domain classes (Doctor, Appointment, Patient, TimeSlot)

---
+++ Domain/Appointment.cs
@@ -0,0 +1,24 @@
+namespace Domain
+{
+    public class Appointment
+    {
+        public string? Reason { get; set; };
+        public string? Note { get; set; };
+        public Patient Patient { get; set; };
+    }
+
+    public void CreateAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+}
