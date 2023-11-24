6896e46d16274931ae4b06500bb421d9a2839b91
Victor Heyse <victor.heyse@student.hogent.be>
2023-11-24 14:32:34 +0100

    Added domain classes (Doctor, Appointment, Patient, TimeSlot)

--- Domain/Doctor.cs
+++ Domain/Doctor.cs
@@ -2,10 +2,57 @@ namespace Domain
 {
     public class Doctor
     {
-        public string Name;
-        public string Gender;
-        public string Specialization;
-        public string Biograph;
-        public string isAvailable;
+        public string Name { get; set; };
+        public string Gender { get; set; };
+        public string Specialization { get; set; };
+        public string Biograph { get; set; };
+        public string IsAvailable { get; set; };
+        public List<TimeSlot> TimeSlots { get; set; };
+        public List<ScheduleTimeSlot> ScheduleTimeSlots { get; set; };
+    }
+
+    public void IsDoctorAvailable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void HasAvailableTimeSlots()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AddScheduleTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateScheduleTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteScheduleTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AddTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteTimeSlot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CreateForcedAppointment()
+    {
+        throw new System.NotImplementedException();
     }
 }
---
+++ Domain/TimeSlot.cs
@@ -0,0 +1,30 @@
+namespace Domain
+{
+    public class TimeSlot
+    {
+        public DateTime DateTime { get; set; };
+        public int Duration { get; set; };
+        public AppointmentType AppointmentType { get; set; };
+        public Appointment Appointment { get; set; };
+    }
+
+    public Boolean IsTimeSlotAvailable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AddAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeleteAppointment()
+    {
+        throw new System.NotImplementedException();
+    }
+}
