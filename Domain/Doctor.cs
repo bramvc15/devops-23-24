705798449b2ad292caf8d3975b47cf357aa26364
Skippn <landerdekesel@gmail.com>
2023-11-24 14:46:51 +0100

    domain code refactor

--- Domain/Doctor.cs
+++ Domain/Doctor.cs
@@ -1,58 +1,57 @@
-namespace Domain
+namespace Domain;
+
+public class Doctor
 {
-    public class Doctor
-    {
-        public string Name { get; set; }
-        public Gender Gender { get; set; }
-        public string Specialization { get; set; }
-        public string Biograph { get; set; }
-        public string IsAvailable { get; set; }
-        public List<TimeSlot> TimeSlots { get; set; }
-        public List<ScheduleTimeSlot> ScheduleTimeSlots { get; set; }
-    }
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+    public string Specialization { get; set; }
+    public string Biograph { get; set; }
+    public string IsAvailable { get; set; }
+    public List<TimeSlot> TimeSlots { get; set; }
+    public List<ScheduleTimeSlot> ScheduleTimeSlots { get; set; }
 
     public void IsDoctorAvailable()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void HasAvailableTimeSlots()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void AddScheduleTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void UpdateScheduleTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void DeleteScheduleTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void AddTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void UpdateTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void DeleteTimeSlot()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void CreateForcedAppointment()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 }
--- Domain/TimeSlot.cs
+++ Domain/TimeSlot.cs
@@ -1,30 +1,29 @@
-namespace Domain
+namespace Domain;
+
+public class TimeSlot
 {
-    public class TimeSlot
-    {
-        public DateTime DateTime { get; set; }
-        public int Duration { get; set; }
-        public AppointmentType AppointmentType { get; set; }
-        public Appointment Appointment { get; set; }
-    }
+    public DateTime DateTime { get; set; }
+    public int Duration { get; set; }
+    public AppointmentType AppointmentType { get; set; }
+    public Appointment Appointment { get; set; }
 
     public Boolean IsTimeSlotAvailable()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void AddAppointment()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void UpdateAppointment()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void DeleteAppointment()
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 }
