590f74f645fdfd2e80c4e88088728662233d42b8
Skippn <landerdekesel@gmail.com>
2023-11-24 14:41:01 +0100

    domain changes

---
+++ Domain/ScheduleTimeSlot.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class ScheduleTimeSlot
+{
+    public AppointmentType AppointmentType { get; set; }
+    public DateTime DateTime { get; set; }
+    public string DayOfWeek { get; set; }
+    public int Duration { get; set; }
+}
