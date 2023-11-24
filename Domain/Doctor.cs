736954df975f2f17d4bcf698664930735b91bc44
Victor Heyse <victor.heyse@student.hogent.be>
2023-11-24 14:09:25 +0100

    Added first domain class

---
+++ Domain/Doctor.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class Doctor
+    {
+        public string Name;
+        public string Gender;
+        public string Specialization;
+        public string Biograph;
+        public string isAvailable;
+    }
+}
