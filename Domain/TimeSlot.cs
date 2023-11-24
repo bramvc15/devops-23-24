79b98da98cf8cb3382cb62e2409c673d82a751c6
Skippn <landerdekesel@gmail.com>
2023-11-24 14:41:01 +0100

    domain changes

--- Domain/TimeSlot.cs
+++ Domain/TimeSlot.cs
@@ -2,10 +2,10 @@ namespace Domain
 {
     public class TimeSlot
     {
-        public DateTime DateTime { get; set; };
-        public int Duration { get; set; };
-        public AppointmentType AppointmentType { get; set; };
-        public Appointment Appointment { get; set; };
+        public DateTime DateTime { get; set; }
+        public int Duration { get; set; }
+        public AppointmentType AppointmentType { get; set; }
+        public Appointment Appointment { get; set; }
     }
 
     public Boolean IsTimeSlotAvailable()
