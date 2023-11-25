2fe3ea2519195b3cc8535acbd77eebbf7c0e0838
Skippn <landerdekesel@gmail.com>
2023-11-25 10:59:17 +0100

    validation and method implementation

--- Domain/Patient.cs
+++ Domain/Patient.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Domain;
 
 public class Patient
@@ -30,7 +32,12 @@ public class Patient
         private set
         { 
             if(string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Email cannot be empty");
-            // TODO additional email validation
+
+            if (!IsValidEmail(value))
+            {
+                throw new ArgumentException("Invalid email format");
+            }
+
             _email = value;
         }
     }
@@ -42,8 +49,13 @@ public class Patient
         private set
         { 
             if(string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("Phone number cannot be empty");
+
+            if (!IsValidPhoneNumber(value))
+            {
+                throw new ArgumentException("Invalid phone number format");
+            }
+
             _phoneNumber = value;
-            // TODO additional phone number validation
         }
     }
     public DateTime DateOfBirth {
@@ -53,8 +65,18 @@ public class Patient
         }
         private set
         { 
-            if(value == null) throw new ArgumentNullException("Date of birth cannot be empty");
-            // TODO additional date of birth validation
+            if (value == default(DateTime)) throw new ArgumentNullException("Date of birth cannot be empty");
+
+            if (value > DateTime.Now)
+            {
+                throw new ArgumentException("Date of birth cannot be in the future");
+            }
+
+            if (DateTime.Now.Year - value.Year > 150)
+            {
+                throw new ArgumentException("Date of birth is too far in the past");
+            }
+
             _dateOfBirth = value;
         }
     }
@@ -78,9 +100,32 @@ public class Patient
     {
         return _appointments;
     }
-        public void MakeAppointment()
+
+    public void MakeAppointment(string reason, string note)
     {
-        // TODO implement
+        Appointment appointment = new(this, reason, note);
+        _appointments.Add(appointment);
+    }
+    #endregion
+
+    #region ValidationMethods
+    private static bool IsValidEmail(string email)
+    {
+        // checks whether the email has the structure of 'username@domain.extension' where:
+        // 'username' can include letters, digits, dots ('.'), underscores ('_'), and hyphens ('-')
+        // 'domain' can include letters, digits, dots ('.'), and hyphens ('-')
+        // 'extension' should have at least two letters
+        string emailRegex = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        Regex regex = new(emailRegex);
+        return regex.IsMatch(email);
+    }
+
+    private static bool IsValidPhoneNumber(string phoneNumber)
+    {
+        // check whether the provided phone number consists only of digits and, optionally, starts with a plus sign ('+')
+        string phoneRegex = @"^\+?[0-9]*$";
+        Regex regex = new(phoneRegex);
+        return regex.IsMatch(phoneNumber);
     }
     #endregion
 }
