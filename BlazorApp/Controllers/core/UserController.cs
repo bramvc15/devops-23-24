8ee9ad8291adcbe477e5e076f541d55320fc8691
Skippn <landerdekesel@gmail.com>
2023-12-18 16:55:41 +0100

    werkt niet!!!! keb geen idee

---
+++ BlazorApp/Controllers/core/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Auth0.ManagementApi;
+using Auth0.ManagementApi.Models;
+using Auth0.ManagementApi.Paging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTO.Core;
+
+namespace BlazorApp.Controllers.core;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize(Roles = "Admin")]
+
+public class UserController : ControllerBase
+{
+    private readonly IManagementApiClient _managementApiClient;
+
+    public UserController(IManagementApiClient managementApiClient)
+    {
+        _managementApiClient = managementApiClient;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<UserDTO.Index>> GetUsers()
+    {
+        var users = await _managementApiClient.Users.GetAllAsync(new GetUsersRequest(), new PaginationInfo());
+        return users.Select(x => new UserDTO.Index
+        {
+            Email = x.Email,
+            FirstName = x.FirstName,
+            LastName = x.LastName,
+            Blocked = x.Blocked ?? false,
+        });
+    }
+}
---
+++ Shared/DTO/Core/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.DTO.Core;
+
+public static class UserDTO
+{
+    public class Index
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Blocked { get; set; }
+    }
+}
