bde53274379336bb5620aaef650c4194078cc887
Thomas Dewilde <thomas.dewilde@student.hogent.be>
2023-12-20 22:36:13 +0100

    ter voorbeeld

--- Persistence/Data/DbInitializer.cs
+++ Persistence/Data/DbInitializer.cs
@@ -247,6 +247,10 @@ namespace Persistence.Data
             var messages = new Message[]
             {
                 new Message("John", "Doe", "john@mail.com", "0489481512", new DateTime(1990, 1, 1), "This is the first message!", false),
+                new Message("Thommy", "Shelby", "thommy@mail.com", "0489481512", new DateTime(1990, 1, 1), "Ik hoop dat dit berichtje u bereikt te midden van uw drukke dagen. Terwijl de feestdagen dichterbij komen, vroeg ik me af of u een momentje tijd heeft om mijn wens te horen...", false),
+                new Message("Tow", "Mater", "tow@mail.com", "0489481512", new DateTime(1990, 1, 1), "Ik word steeds roestiger en roestiger. Voro deze reden kan ik ook niet zo goed meer autorijden. Kunt u me helpen?", false),
+                new Message("Buzz", "Lightyear", "buzz@mail.com", "0489481512", new DateTime(1990, 1, 1), "Naar de sterren, en er dan voorbij!", false),
+                new Message("Jack", "Sparrow", "jack@mail.com", "0489481512", new DateTime(1990, 1, 1), "The beste piraat deze wereld ooit gezien heeft! Ik hou van wiskey ook.", false),
             };
             #endregion
 
