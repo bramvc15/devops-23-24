de95e5b5758f8afe7894c4bada784ae2c9957c75
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2023-12-23 21:27:27 +0100

    E2E testen

--- Tests/IntegrationTests/Tests/NavigationTest.cs
+++ Tests/IntegrationTests/Tests/NavigationTest.cs
@@ -1,16 +1,13 @@
 using Microsoft.Playwright.NUnit;
 using Microsoft.Playwright;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
 
 namespace IntegrationTests;
 
 [Parallelizable(ParallelScope.Self)]
 public class NavigationTest : PageTest
 {
-    // debug: $env:PWDEBUG=0
-    // run tests command: dotnet test
-    // bin/Debug/net6.0/playwright.ps1 codegen 192.168.0.123:5046  
-
     [SetUp]
     public async Task SetUp()
     {
@@ -54,14 +51,10 @@ public class NavigationTest : PageTest
         Assert.IsTrue(await Page.GetByRole(AriaRole.Link, new() { Name = "Log in" }).IsVisibleAsync());
     }
 
-    // [Test]
-    // public async Task LoggedIn_AdminRedirectsToAdminPage()
-    // {
-    //     await Page.GotoAsync($"{baseUrl}/login");
-    //     await Page.GetByPlaceholder("Naam").FillAsync("admin1");
-    //     await Page.GetByPlaceholder("Wachtwoord").FillAsync("admin1");
-    //     await Page.ClickAsync("button:has-text(\"Login\")");
-    //     await Page.GotoAsync($"{baseUrl}/admin");
-    //     Assert.AreEqual($"{baseUrl}/admin", Page.Url);
-    // }
+    [Test]
+    public async Task Login_RedirectsToAuth0()
+    {
+        await Page.GotoAsync($"{TestHelper.BaseUrl}login");
+        await Expect(Page).ToHaveURLAsync(new Regex(@"https://vision-oogcentrum\.eu\.auth0\.com/.*"));
+    }
 }
