52cdce979747ef91d374f5d05b756a78ec3ea345
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2023-12-24 12:45:02 +0100

    e2e testhelper change

--- Tests/IntegrationTests/TestHelper.cs
+++ Tests/IntegrationTests/TestHelper.cs
@@ -10,9 +10,9 @@ namespace IntegrationTests
         // dotnet test
 
         // Codegen:
-        // bin/Debug/net6.0/playwright.ps1 codegen 192.168.0.123:5046  
+        // bin/Debug/net6.0/playwright.ps1 codegen {IP HERE}
 
         // BaseUrl inclusief / op het einde
-        public static string BaseUrl = "http://192.168.0.123:5002/";
+        public static string BaseUrl = "https://localhost:5001/";
     }
 }
