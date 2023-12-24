eb953e7ffe904c9e6831e368380c536d95f1b93a
VictorHeyse <92031273+VictorHeyse@users.noreply.github.com>
2023-12-24 12:34:02 +0100

    Added last unit tests

---
+++ Tests/Domain.Tests/NoteTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain.Tests;
+
+public class NoteTest
+{
+    #region Fields
+    private readonly Note _note;
+    #endregion
+
+    #region Init
+    public NoteTest()
+    {
+        _note = new Note("question!", "answer!!");
+    }
+    #endregion
+
+    #region Constructor tests
+    [Fact]
+    public void Note_Constructor_ValidParameters()
+    {
+        new Note("question!", "answer!!");
+    }
+
+    [Fact]
+    public void Note_Constructor_EmptyTitle_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new Note("", "answer!!"));
+    }
+
+    [Fact]
+    public void Note_Constructor_EmptyContext_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new Note("question!", ""));
+    }
+    #endregion
+
+    #region Method tests
+    [Fact]
+    public void Note_UpdateNote()
+    {
+        _note.UpdateNote("new", "new2");
+        Assert.Equal(_note.Title, "new");
+        Assert.Equal(_note.Content, "new2");
+    }
+    #endregion
+}
---
+++ Tests/Domain.Tests/TreatmentTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Domain.Tests;
+
+public class TreatmentTest
+{
+    #region Fields
+    private readonly Treatment _treatment;
+    #endregion
+
+    #region Init
+    public TreatmentTest()
+    {
+        _treatment = new Treatment("name", "description", "link");
+    }
+    #endregion
+
+    #region Constructor tests
+    [Fact]
+    public void Treatment_Constructor_ValidParameters()
+    {
+        new Treatment("name", "description", "link");
+    }
+
+    [Fact]
+    public void Treatment_Constructor_EmptyName_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new Treatment("", "description", "link"));
+    }
+
+    [Fact]
+    public void Treatment_Constructor_EmptyDescription_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new Treatment("name", "", "link"));
+    }
+
+    [Fact]
+    public void Treatment_Constructor_EmptyImageLink_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new Treatment("name", "description", ""));
+    }
+    #endregion
+
+    #region Method tests
+    [Fact]
+    public void Treatment_UpdateTreatment()
+    {
+        _treatment.UpdateTreatment("new", "new2", "new3");
+        Assert.Equal(_treatment.Name, "new");
+        Assert.Equal(_treatment.Description, "new2");
+        Assert.Equal(_treatment.ImageLink, "new3");
+    }
+    #endregion
+}
